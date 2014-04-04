06f1031af3951c7ece2787b37a4825da5ca2c18b
tmtxt <tmtxt@tmtxt-mac.local>
2014-04-04 11:03:21 +0700

    update command for starting aria2

--- aria2/start-aria2.sh
+++ aria2/start-aria2.sh
@@ -1,3 +1,1 @@
-cd ~/Downloads
-touch session.txt
-aria2c --enable-rpc --rpc-listen-all --save-session=session.txt -isession.txt -x16 -k1M
+touch $HOME/Downloads/session.txt && aria2c --enable-rpc --rpc-listen-all --save-session=$HOME/Downloads/session.txt --input-file=$HOME/Downloads/session.txt -x16 -s16 -k1M --dir=$HOME/Downloads --daemon --on-download-complete $HOME/bin/aria2-download-complete.sh
