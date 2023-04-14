da3e655344014a3127ff1a2491a172b95bc84b83
TruongTX <me@truongtx.me>
2023-04-14 14:08:41 +1000

    Update codespaces script

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@
 mkdir ~/projects
 git clone https://github.com/tmtxt/dotfiles.git ~/projects/dotfiles
 
+# git editor
+git config --global core.editor "code --wait"
+
 # remove default .zshrc file
 mv ~/.zshrc ~/.zshrc-default
 ln -s $HOME/projects/dotfiles/.zshrc ~/.zshrc
