aba69c4090dd73aba0ec85682c4bfd509d2cb9cd
Tony Tran <tony.tran@wisetechglobal.com>
2025-02-13 14:45:20 +1100

    Add dev tool commands

--- Powershell/Microsoft.PowerShell_profile_wisetech.ps1
+++ Powershell/Microsoft.PowerShell_profile_wisetech.ps1
@@ -8,6 +8,22 @@ Import-Module ZLocation
 # Write-Host -Foreground Green "`n[ZLocation] knows about $((Get-ZLocation).Keys.Count) locations.`n"
 Set-Alias -Name j -Value Invoke-ZLocation
 
+function Read-MultiLineInput {
+    $inputLines = @()
+    $endMarker = ""
+    Write-Host "Enter multiple lines of input (type 'Ctrl+D and then Enter' on a new line to finish):"
+
+    while ($true) {
+        $line = Read-Host
+        if ($line -eq $endMarker) {
+            break
+        }
+        $inputLines += $line
+    }
+
+    return $inputLines -join "`n"
+}
+
 Function uuidv4 {
     (New-Guid).Guid.ToUpper() | Set-Clipboard
     Write-Output "New uuidv4 (upper-case) copied to clipboard"
@@ -27,4 +43,54 @@ function randomstring {
     $random = -join ((65..90) + (97..122) + (48..57) | Get-Random -Count $length | ForEach-Object { [char]$_ })
     $random | Set-Clipboard
     return $random
+}
+
+function lowercase {
+    Write-Host "Please enter the string to convert to lowercase and copy to clipboard:"
+    $InputString = Read-Host
+    $lowerCaseString = $InputString.ToLower()
+    $lowerCaseString | Set-Clipboard
+    Write-Host "The converted string has been copied to the clipboard."
+}
+
+function uppercase {
+    Write-Host "Please enter the string to convert to uppercase and copy to clipboard:"
+    $InputString = Read-Host
+    $lowerCaseString = $InputString.ToUpper()
+    $lowerCaseString | Set-Clipboard
+    Write-Host "The converted string has been copied to the clipboard."
+}
+
+function base64encode {
+    Write-Host "Please enter the string to encode to Base64 and copy to clipboard:"
+    $InputString = Read-MultiLineInput
+    $bytes = [System.Text.Encoding]::UTF8.GetBytes($InputString)
+    $base64String = [Convert]::ToBase64String($bytes)
+
+    if ($base64String.Length -le 100) {
+        Write-Host "Base64 string: $base64String" -ForegroundColor Yellow
+    } else {
+        $first100Chars = $base64String.Substring(0, [Math]::Min(100, $base64String.Length))
+        Write-Host "Truncated base64 string: $first100Chars..." -ForegroundColor Yellow
+    }
+
+    $base64String | Set-Clipboard
+    Write-Host "The encoded string has been copied to the clipboard."
+}
+
+function base64decode {
+    Write-Host "Please enter the Base64 encoded string to decode and copy to clipboard:"
+    $InputString = Read-MultiLineInput
+    $bytes = [Convert]::FromBase64String($InputString)
+    $decodedString = [System.Text.Encoding]::UTF8.GetString($bytes)
+
+    if ($decodedString.Length -le 100) {
+        Write-Host "Decoded string: $decodedString" -ForegroundColor Yellow
+    } else {
+        $first100Chars = $decodedString.Substring(0, [Math]::Min(100, $decodedString.Length))
+        Write-Host "Truncated decoded string: $first100Chars..." -ForegroundColor Yellow
+    }
+
+    $decodedString | Set-Clipboard
+    Write-Host "The decoded string has been copied to the clipboard."
 }
