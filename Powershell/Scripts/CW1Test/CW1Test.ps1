e12d4185574654eb1bd21c62621a1cd3589cc4ba
Tony Tran <tony.tran@wisetechglobal.com>
2025-12-07 11:09:46 +1100

    Organize scripts

--- Powershell/Scripts/CW1Test/CW1Test.ps1
+++ Powershell/Scripts/CW1Test/CW1Test.ps1
@@ -1,26 +1,19 @@
+# Script to trigger CW1 from Jetbrains Rider
 param (
   [string]$FilePath,
   [int]$LineNumber
 )
 
-# dotnet --list-sdks
-
-# Write-Host $FilePath
-# Write-Host $LineNumber
-
-# Run first sub-command (e.g., ProjectFinder)
 Push-Location $PSScriptRoot
 $projectPath = & dotnet run ".\ProjectFinder.cs" $FilePath
-
-# Run second sub-command (e.g., MethodFinder)
 $methodName = & dotnet run ".\MethodFinder.cs" -- $FilePath $LineNumber
 Pop-Location
 
-# Use both outputs in another command (example: echo)
+Write-Output "Running unit test..."
 Write-Output "Project: $projectPath"
 Write-Output "Method: $methodName"
 Write-Output ""
-# Write-Output "Combined: $projectPath :: $methodName"
+
 Push-Location "C:\git\GitHub\WiseTechGlobal\CargoWise"
 dotnet test $projectPath --filter "FullyQualifiedName~$methodName&CapabilityRequirements~NET48"
 Pop-Location
